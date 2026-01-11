d4d89f2e83aacf84d41c43d81925034e87b50d49
vijayakumar58 <vijayalingam58@gmail.com>
2026-01-11 11:52:44 +0530

    add

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.listen(process.env.PORT || 3000);
 //middleware
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3001"
+    // origin: "http://localhost:3001"
+    origin: "passwordresetemail.netlify.app"
 }))
 
 const Authenticate = (req, res, next) => {
